2b74a2bb9944c804fcf25119e32365f98015e551
AndreyDu1986 <anddu391986@gmail.com>
2024-11-07 17:39:09 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,6 @@
+my_string=input('Как вас зовут? ')
+my_string=input('Как вас зовут? '. upper())
+my_string=input('Как вас зовут? '.lower())
+my_string=input('Как вас зовут? '.replace(' ', ''))
+my_string=input('Как вас зовут? '.replace('Как вас зовут?', 'К'))
+my_string=input('Как вас зовут? '.replace('Как вас зовут?', '?'))
