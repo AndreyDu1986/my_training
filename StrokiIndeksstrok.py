7f41a16906a51e034de2d64ca5e7ececf4a544ac
AndreyDu1986 <anddu391986@gmail.com>
2024-11-07 16:42:23 +0300

    Add files via upload

---
+++ StrokiIndeksstrok.py
@@ -0,0 +1,11 @@
+name='example'
+print(name[0])
+print(name[-1])
+print(name[4:7])
+print(name[::-1])
+example='Топинамбур'
+print(example [0])
+print(example [-1])
+print(example [5:10])
+print(example [::-1])
+print(example [1:10:2])
