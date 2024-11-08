fdc890b566df76f2938969fad4d43be53606d81c
AndreyDu1986 <anddu391986@gmail.com>
2024-11-08 10:17:41 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var=1,2,3,4,5
+print(immutable_var)
+#immutable_var[0]=100
+#print(immutable_var) # нельзя вывести отдельный элемент из кортежа
+mutable_list=(1,2,3,4,5)+(1,2)
+print(mutable_list)
