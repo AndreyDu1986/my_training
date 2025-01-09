8fd888f37222b2e59da7514aa3e616f7edf0476e
AndreyDu1986 <anddu391986@gmail.com>
2025-01-09 11:57:34 +0300

    Add files via upload

---
+++ module_7_4.py
@@ -0,0 +1,24 @@
+team1_num = 5
+output_string = "В команде Мастера кода участников: %d !" % team1_num
+print(output_string)
+team1_num = 5
+team2_num = 6
+output_string = "Итого сегодня в командах участников: %d и %d !" % (team1_num, team2_num)
+print(output_string)
+score_2 = 42
+output_string = "Команда Волшебники данных решила задач: {} !".format(score_2)
+print(output_string)
+team1_time = 18015.2
+output_string = "Волшебники данных решили задачи за {:.1f} с !".format(team1_time)
+print(output_string)
+score_1 = 40
+score_2 = 42
+output_string = f"Команды решили {score_1} и {score_2} задач."
+print(output_string)
+challenge_result = 'Победа команды Мастера кода!'
+output_string = f"Результат битвы: {challenge_result}"
+print(output_string)
+tasks_total = 82
+time_avg = 350.4
+output_string = f"Сегодня было решено {tasks_total} задач, в среднем по {time_avg:.1f} секунды на задачу!."
+print(output_string)
