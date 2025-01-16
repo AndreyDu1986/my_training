dc90ef85f9395daa539612696a7af91b8661c9b0
AndreyDu1986 <anddu391986@gmail.com>
2025-01-16 09:51:36 +0300

    Add files via upload

--- module_9_1.py
+++ module_9_1.py
@@ -1,13 +1,9 @@
 def apply_all_func(int_list, *functions):
-    results = {}
-    for func in functions:
-        try:
-            result = func(int_list)
-            results[func.__name__] = result
-        except Exception as e:
-            print(f"Возникло исключение: {e}")
-            continue
-    return results
+    result = {}
+
+    for i in functions:
+        result[i.__name__] = i(int_list)
+    return result
 
 print(apply_all_func([6, 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
